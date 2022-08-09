3107f3e2ac5652f3fe998d172b046736cf78de26
Serhii Leonenko <leonenko3149@gmail.com>
2022-08-09 20:11:22 +0300

    first_commit

---
+++ statistic/management/commands/bot.py
@@ -0,0 +1,79 @@
+import telebot
+from django.contrib.auth import get_user_model
+from django.core.management import BaseCommand
+from telebot import types
+
+
+class Command(BaseCommand):
+    help = "telegram-bot"
+
+    def handle(self, *args, **options):
+        bot = telebot.TeleBot("5502151656:AAE7G6LZVaHL9bKhmf1G-6BBKabwu8IsSmw")
+
+        valid_users = {
+            "bogdan": 1799244985,
+            "lovelas": 1836086969,
+        }
+
+        @bot.message_handler(commands=["start"])
+        def start(message):
+            bot.send_message(
+                message.chat.id,
+                "Hello!\npush this <b>/button</b> command",
+                parse_mode="html",
+            )
+
+        @bot.message_handler(commands=["button"])
+        def button_message(message):
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item1 = types.KeyboardButton("Registration")
+            markup.add(item1)
+            bot.send_message(message.chat.id, "Push registration", reply_markup=markup)
+
+        @bot.message_handler(content_types=["text"])
+        def message_reply(message):
+            username = message.from_user.username
+            user_id = message.from_user.id
+            if message.text == "Registration":
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                item1 = types.KeyboardButton("Get credentials")
+                markup.add(item1)
+                if user_id in valid_users.values():
+                    user, created = get_user_model().objects.get_or_create(
+                        username=username
+                    )
+                    if created:
+                        user.set_password(str(user_id))
+                        user.save()
+                        bot.send_message(
+                            message.chat.id,
+                            "Registration successfully!",
+                            reply_markup=markup,
+                        )
+                    elif user:
+                        bot.send_message(
+                            message.chat.id,
+                            "You've already registered!",
+                            reply_markup=markup,
+                        )
+                else:
+                    bot.send_message(
+                        message.chat.id,
+                        "You can't be registered, credentials are not valid",
+                    )
+            elif message.text == "Get credentials":
+                inline_markup = types.InlineKeyboardMarkup()
+                inline_markup.add(
+                    types.InlineKeyboardButton(
+                        "Go to the website...",
+                        url="http://127.0.0.1:8000/accounts/login/",
+                    )
+                )
+                bot.send_message(
+                    message.chat.id,
+                    f"'username': {username}\n'password': {user_id}\n🚀",
+                    reply_markup=inline_markup,
+                )
+
+        bot.infinity_polling()
+        bot.polling(none_stop=True)
---
+++ statistic/views.py
@@ -0,0 +1,52 @@
+import datetime
+import os
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+import requests
+from app.settings import TIME_ZONE
+from statistic.forms import DateForm
+
+
+@login_required
+def index(request):
+    url = os.getenv("PARSE_URL")
+    headers = {"Api-Key": os.getenv("API_KEY")}
+
+    today = datetime.datetime.today()
+    start_date = today.isoformat()
+    end_date = today.isoformat()
+
+    if request.method == "POST":
+        form = DateForm(request.POST)
+        if form.is_valid():
+            start_date = form.cleaned_data["start_date"].isoformat()
+            end_date = form.cleaned_data["end_date"].isoformat()
+    else:
+        form = DateForm()
+
+    data = {
+        "range": {
+            "from": start_date,
+            "to": end_date,
+            "timezone": TIME_ZONE,
+            "interval": "custom_date_range",
+        },
+        "metrics": ["clicks", "conversions"],
+        "filters": [
+            {"name": "sub_id_6", "operator": "CONTAINS", "expression": "bogdan"}
+        ],
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    content = response.json()["rows"][0]
+    clicks = content["clicks"]
+    conversions = content["conversions"]
+
+    context = {
+        "clicks": clicks,
+        "conversions": conversions,
+        "form": form,
+    }
+
+    return render(request, "statistic/index.html", context=context)
